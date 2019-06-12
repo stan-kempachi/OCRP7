1b4f1e7b2a0f948326be3e2e1af01fdddd51d2f5
stan-kempachi <35777602+stan-kempachi@users.noreply.github.com>
2019-06-12 16:46:16 +0200

    correctif crash heroku 3, suppression fichier tests

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn grandpybot:app
-init: FLASK_APP=run.py flask init_db
--- config.py
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env python
-
-SECRET_KEY = '#d#JCqTTW\nilK\\7m\x0bp#\tj~#H'
-
-MAP_API_KEY = 'AIzaSyB9GoIGEjTsNVmaYSfuhcDQ9OxU9udjjYY'
-
