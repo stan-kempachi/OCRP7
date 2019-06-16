73a4bddb2c5baae1c9058719618159c67ae217c8
stan-kempachi <35777602+stan-kempachi@users.noreply.github.com>
2019-06-16 21:47:23 +0200

    PEP8 app.py

--- grandpybot/app.py
+++ grandpybot/app.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
-
+"""
+File containing function main
+"""
 import random
 from datetime import datetime
 
@@ -13,32 +15,36 @@ from grandpybot.wiki_class import Wikipedia
 
 app = Flask(__name__)
 
-# Config options - Make sure you created a 'config.py' file.
+"""Config option """
 app.config.from_object('config')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
 @app.context_processor
 def inject_now():
+    """return datetime"""
     return dict(now=datetime.now())
 
 @app.route('/index', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    """define homepage"""
     return render_template('pages/home.html', google_key=conf.MAP_API_KEY)
 
 @app.errorhandler(404)
 def page_not_found(error):
+    """define error page"""
     return render_template('errors/404.html'), 404
+
 @app.route('/get_user_request', methods=['GET'])
 def get_user_request():
+    """define get user request methode"""
     if request.method == "GET":
         request_user = request.args.get('question')
         parser = Parser(request_user)
         information_extracted = parser.extract_information_request(request_user)
         information_extracted = parser.remove_punctuation(information_extracted)
         information_extracted = parser.remove_word_please((information_extracted))
-        print(information_extracted)
         type_search = information_extracted['type_search']
         information = information_extracted['information']
         wiki = Wikipedia()
